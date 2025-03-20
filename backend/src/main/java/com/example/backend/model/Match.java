caab49eb3d9525090968a77e3b34a606142d6980
francesco.maxim <francesco.maxim@icloud.com>
2025-03-20 07:47:04 +0200

    created match

--- backend/src/main/java/com/example/backend/model/Match.java
+++ backend/src/main/java/com/example/backend/model/Match.java
@@ -1,4 +1,25 @@
 package com.example.backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Match {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private User user1;
+    @OneToOne
+    private User user2;
+    private LocalDateTime matchDate;
 }
