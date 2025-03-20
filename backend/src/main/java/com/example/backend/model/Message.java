9a04c67a197ef6ebc62d674323df5cd47c5f7b03
francesco.maxim <francesco.maxim@icloud.com>
2025-03-20 07:43:24 +0200

    created message

--- backend/src/main/java/com/example/backend/model/Message.java
+++ backend/src/main/java/com/example/backend/model/Message.java
@@ -1,4 +1,27 @@
 package com.example.backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private User userSender;
+    @OneToOne
+    private User userReceiver;
+
+    private String content;
+    private LocalDateTime dateTime;
 }
