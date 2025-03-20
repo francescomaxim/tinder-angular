5f4b696aaa7679e7bf8e37bcc3288f5d9969050d
francesco.maxim <francesco.maxim@icloud.com>
2025-03-20 07:35:03 +0200

    created user

--- backend/src/main/java/com/example/backend/model/User.java
+++ backend/src/main/java/com/example/backend/model/User.java
@@ -1,4 +1,23 @@
 package com.example.backend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String password;
+    @OneToOne
+    private Profile profile;
+
 }
